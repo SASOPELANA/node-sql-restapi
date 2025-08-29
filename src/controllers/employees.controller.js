ffb524c7f992af9295457d978700dc81ff7b08d3
Sergio Dev <sopekof@gmail.com>
2025-08-29 15:02:35 -0300

    Manejo de errores

--- src/controllers/employees.controller.js
+++ src/controllers/employees.controller.js
@@ -1,70 +1,99 @@
 import { pool } from "../db.js";
 
 export const getEmployees = async (req, res) => {
-	const [rows] = await pool.query("SELECT * FROM employee");
-	res.send(rows);
+	try {
+		// linea para test y manejos de errores
+		//throw new Error("DB error");
+		const [rows] = await pool.query("SELECT * FROM employee");
+		res.send(rows);
+	} catch (error) {
+		return res.status(500).json({ message: "Something goes wrong" });
+	}
 };
 
 export const getEmployee = async (req, res) => {
-	console.log(req.params.id);
-	const [rows] = await pool.query("SELECT * FROM employee WHERE id = ?", [
-		req.params.id,
-	]);
+	try {
+		//console.log(req.params.id);
+		//throw new Error("DB error");
+		const [rows] = await pool.query("SELECT * FROM employee WHERE id = ?", [
+			req.params.id,
+		]);
 
-	if (rows.length <= 0)
-		return res.status(404).json({
-			message: "Employee not found",
-		});
+		if (rows.length <= 0)
+			return res.status(404).json({
+				message: "Employee not found",
+			});
 
-	res.json(rows[0]);
+		res.json(rows[0]);
+	} catch (error) {
+		return res.status(500).json({ message: "Something goes wrong" });
+	}
 };
 
 export const createEmployee = async (req, res) => {
-	const { name, apellido, salary } = req.body;
-	const [rows] = await pool.query(
-		"INSERT INTO employee (name,apellido,salary) VALUES(?,?,?) ",
-		[name, apellido, salary],
-	);
+	try {
+		//throw new Error("DB error");
+		const { name, apellido, salary } = req.body;
+		const [rows] = await pool.query(
+			"INSERT INTO employee (name,apellido,salary) VALUES(?,?,?) ",
+			[name, apellido, salary],
+		);
 
-	res.send({
-		id: rows.insertId,
-		name,
-		apellido,
-		salary,
-	});
+		res.send({
+			id: rows.insertId,
+			name,
+			apellido,
+			salary,
+		});
+	} catch (error) {
+		return res.status(500).json({ message: "Something goes wrong" });
+	}
 };
 
 export const deleteEmployee = async (req, res) => {
-	const [result] = await pool.query("DELETE FROM employee WHERE id = ?", [
-		req.params.id,
-	]);
-	//console.log(result);
+	try {
+		//throw new Error("DB error");
+		const [result] = await pool.query("DELETE FROM employee WHERE id = ?", [
+			req.params.id,
+		]);
+		//console.log(result);
 
-	if (result.affectedRows <= 0)
-		return res.status(404).json({
-			message: "Employee not found.",
-		});
+		if (result.affectedRows <= 0)
+			return res.status(404).json({
+				message: "Employee not found.",
+			});
 
-	res.sendStatus(200);
+		res.sendStatus(200);
+	} catch (error) {
+		return res.status(500).json({ message: "Something goes wrong" });
+	}
 };
 
 export const updateEmployee = async (req, res) => {
-	const { id } = req.params;
+	try {
+		//throw new Error("DB error");
 
-	const { name, apellido, salary } = req.body;
+		const { id } = req.params;
 
-	// Metodo de mysql para actualizar datos que no sean nulos
-	const [result] = await pool.query(
-		"UPDATE employee SET name = IFNULL(?,name), apellido =  IFNULL(?,apellido), salary = IFNULL(?,salary) WHERE id = ? ",
-		[name, apellido, salary, id],
-	);
+		const { name, apellido, salary } = req.body;
 
-	if (result.affectedRows <= 0)
-		return res.status(404).json({ message: "Employee not found." });
+		// Metodo de mysql para actualizar datos que no sean nulos
+		const [result] = await pool.query(
+			"UPDATE employee SET name = IFNULL(?,name), apellido =  IFNULL(?,apellido), salary = IFNULL(?,salary) WHERE id = ? ",
+			[name, apellido, salary, id],
+		);
 
-	//console.log(result);
+		if (result.affectedRows <= 0)
+			return res.status(404).json({ message: "Employee not found." });
 
-	const [rows] = await pool.query("SELECT * FROM employee WHERE id = ?", [id]);
+		//console.log(result);
 
-	res.json(rows[0]);
+		const [rows] = await pool.query("SELECT * FROM employee WHERE id = ?", [
+			id,
+		]);
+
+		res.json(rows[0]);
+	} catch (error) {
+		return res.status(500).json({ message: "Something goes wrong" });
+	}
 };
